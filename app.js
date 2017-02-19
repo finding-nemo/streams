66e9d53daf20247b8d0cc86dd19f4397d1622f42
jp <gaines.johnp@gmail.com>
2017-02-18 16:00:51 -0800

    added writable stream

--- app.js
+++ app.js
@@ -3,6 +3,12 @@
 const fs = require('fs');
 
 let readable = fs.createReadStream(__dirname + '/greet.txt', {'encoding': 'utf8'});
+
+let writable = fs.createWriteStream(__dirname + '/greet2.txt');
+
 readable.on('data', (chunk)=> {
   console.log(chunk);
+  writable.write(chunk);
 });
+
+
