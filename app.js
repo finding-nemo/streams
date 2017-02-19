fd74dc76cd6874723fce422c9c83577d3d9998b9
jp <gaines.johnp@gmail.com>
2017-02-18 16:03:47 -0800

    added pipe

--- app.js
+++ app.js
@@ -6,9 +6,5 @@ let readable = fs.createReadStream(__dirname + '/greet.txt', {'encoding': 'utf8'
 
 let writable = fs.createWriteStream(__dirname + '/greet2.txt');
 
-readable.on('data', (chunk)=> {
-  console.log(chunk);
-  writable.write(chunk);
-});
-
+readable.pipe(writable);
 
