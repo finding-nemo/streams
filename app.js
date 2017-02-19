4f67967ad7c34f95e8064eced47cd8a3f2189988
jp <gaines.johnp@gmail.com>
2017-02-18 16:24:31 -0800

    piped gzip to compressed file

--- app.js
+++ app.js
@@ -2,9 +2,21 @@
 
 const fs = require('fs');
 
+//require in zlib to get access to createGzip method
+const zlib = require('zlib');
+
+//create a gzip object
+let gzip = zlib.createGzip();
+
+//create a stream to send the compressed gzip file
+let compressed = fs.createWriteStream(__dirname + '/compressed.txt');
+
 let readable = fs.createReadStream(__dirname + '/greet.txt', {'encoding': 'utf8'});
 
 let writable = fs.createWriteStream(__dirname + '/greet2.txt');
 
 readable.pipe(writable);
+
+//pipe readable through gzip and then pipe it to compressed
+readable.pipe(gzip).pipe(compressed);
 
